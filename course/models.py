17c2f54315b2a437f53fe223de852d3fd4e2acfa
Tcphuoc <tcphuoc0511@gmail.com>
2025-01-31 17:42:11 +0700

    Feature list courses

--- course/models.py
+++ course/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_description = models.CharField(max_length=200)
+    course_image = models.ImageField(default="images/default_course.jpg",upload_to='images', blank=True)

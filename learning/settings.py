40a83ec5539202c7671b415cd1f0fb1156bd33ec
Tcphuoc <tcphuoc0511@gmail.com>
2025-02-03 11:22:55 +0700

    Convert to MySQL

--- learning/settings.py
+++ learning/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = "learning.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learning_development_database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
